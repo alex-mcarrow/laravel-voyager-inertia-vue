afee7e43da5522ff4e1ae8273ea01851aa8113d6
Muumba <vobark@gmail.com>
2021-10-23 20:00:46 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,8 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+//роуты авторизации, можно выключать на проектах, где нет авторизации пользователей, а только админка
+//TODO причесать базовые шаблоны, чтобы не вылетали при отключении
 require __DIR__.'/auth.php';
 
 //voyager admin
