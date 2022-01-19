2073b555084b2d7542484289117e2457992a992a
vbarkaev <vobark@gmail.com>
2022-01-19 17:40:58 +0300

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    purge: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
 
     theme: {
